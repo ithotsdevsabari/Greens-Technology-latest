630386e5fa614544c6dd5abaa39a0da9e572e0fc
sabarinathan N <sabarinathan25052001@gmail.com>
2024-08-11 00:36:28 +0530

    course image updated

--- src/components/IndexPage/OurCourses.tsx
+++ src/components/IndexPage/OurCourses.tsx
@@ -1,172 +1,3 @@
-// "use client";
-// import React from "react";
-// import Link from "next/link";
-// import Image from "next/image";
-// import cou1 from "../../../public/img/java.webp";
-// import cou2 from "../../../public/img/python.png";
-// import cou3 from "../../../public/img/mern.png";
-// import cou4 from "../../../public/img/FED.png";
-
-// const OurCourses = () => {
-//   return (
-//     <div className="pt-8">
-//       <div className="container">
-//       <div className="section-title">
-//           <h2 className="text-center fs-5">
-//           Our Courses
-//         </h2>
-//             <p>
-//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//               eiusmod tempor incididunt ut labore et dolore magna aliqua.
-//             </p>
-//           </div>
-//         <div className="course_grid mt-4">
-
-//           <div className="">
-//             <Image src={cou1} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">Java Full Stack</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="">
-//             <Image src={cou2} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">Python Full Stack</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="">
-//             <Image src={cou3} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">MERN Stack</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="">
-//             <Image src={cou4} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">Front End</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="">
-//             <Image src={cou2} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">Python Full Stack</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="">
-//             <Image src={cou4} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">Front End</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="">
-//             <Image src={cou1} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">Java Full Stack</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="">
-//             <Image src={cou3} alt="program" className="img-fluid radius_roun" />
-//             <div className="cour_content">
-//               <div className="">
-//                 <h6 className="mb-0">MERN Stack</h6>
-//               </div>
-//               <div className="">
-//                 <Link href="#" className="text-white btn_des">
-//                   Batches
-//                 </Link>
-//                 <Link href="#" className="text-white btn_des1 ms-2">
-//                   Enroll Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="text-center mt-5">
-//             <Link href="#" className="button-def button-3"><span>View All Courses</span></Link>
-//             </div>
-
-//       </div>
-//     </div>
-//   );
-// };
-// export default OurCourses;
 
 "use client";
 
@@ -175,85 +6,194 @@ import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
-import cou1 from "../../../public/img/java.webp";
-import cou2 from "../../../public/img/python.png";
-import cou3 from "../../../public/img/mern.png";
-import cou4 from "../../../public/img/FED.png";
+import awscloud from "../../../public/coursesImages/awscloud.jpg";
+import awsdataengineer from "../../../public/coursesImages/awsdataengineer.png";
+import azurecloud from "../../../public/coursesImages/azurecloud.webp";
+import azuredataengineering from "../../../public/coursesImages/azuredataengineering.jpeg"
+import cloudcomputing from "../../../public/coursesImages/cloudcomputing.jpeg"
+import dataanalytics  from "../../../public/coursesImages/dataanalytics.jpeg";
+import dataengineeringgcp  from "../../../public/coursesImages/dataengineeringgcp.jpeg";
+import  datascience from "../../../public/coursesImages/datascience.jpeg";
+import devops from "../../../public/coursesImages/devops.jpeg";
+import  frontend from "../../../public/coursesImages/frontend.jpeg";
+import  gcpcloud from "../../../public/coursesImages/gcpcloud.png";
+import javafullstack from "../../../public/coursesImages/javafullstack.webp";
+import microsoft365  from "../../../public/coursesImages/microsoft365.jpg";
+import  nodejs from "../../../public/coursesImages/nodejs.png";
+import oracle  from "../../../public/coursesImages/oracle.png";
+import powerapps  from "../../../public/coursesImages/powerapps.jpeg";
+import powerautomate  from "../../../public/coursesImages/powerautomate.jpeg";
+import  powerbi from "../../../public/coursesImages/powerbi.jpeg";
+import python  from "../../../public/coursesImages/python.jpeg";
+import pythonfullstack  from "../../../public/coursesImages/pythonfullstack.jpeg";
+import  reactjs from "../../../public/coursesImages/react.jpeg";
+import scrummaster  from "../../../public/coursesImages/scrummaster.jpg";
+import snowflask  from "../../../public/coursesImages/snowflask.webp";
+import sql  from "../../../public/coursesImages/sql.jpeg";
+import tableau  from "../../../public/coursesImages/tableau.jpeg";
+import webdevelopement  from "../../../public/coursesImages/webdevelopement.jpeg";
+
+
 
 const ourWorksData = [
   {
-    image: cou1,
-    title: "MERN Stack",
+    image: awscloud,
+    title: "AWS Cloud",
+    category: "Cloud Computing",
+    viewDetails: "/",
+  },
+  {
+    image: awsdataengineer,
+    title: "AWS Data Engineer",
+    category: "Data Engineering",
+    viewDetails: "/",
+  },
+  {
+    image: azurecloud,
+    title: "Azure Cloud",
+    category: "Cloud Computing",
+    viewDetails: "/",
+  },
+  {
+    image: azuredataengineering,
+    title: "Azure Data Engineering",
+    category: "Data Engineering",
+    viewDetails: "/",
+  },
+  {
+    image: cloudcomputing,
+    title: "Cloud Computing",
+    category: "Cloud Computing",
+    viewDetails: "/",
+  },
+  {
+    image: dataanalytics,
+    title: "Data Analytics",
+    category: "Data Science",
+    viewDetails: "/",
+  },
+  {
+    image: dataengineeringgcp,
+    title: "GCP Data Engineering",
+    category: "Data Engineering",
+    viewDetails: "/",
+  },
+  {
+    image: datascience,
+    title: "Data Science",
+    category: "Data Science",
+    viewDetails: "/",
+  },
+  {
+    image: devops,
+    title: "DevOps",
     category: "Development",
     viewDetails: "/",
   },
   {
-    image: cou2,
+    image: frontend,
+    title: "Front-end Development",
+    category: "Web Development",
+    viewDetails: "/",
+  },
+  {
+    image: gcpcloud,
+    title: "GCP Cloud",
+    category: "Cloud Computing",
+    viewDetails: "/",
+  },
+  {
+    image: javafullstack,
     title: "Java Full Stack",
-    category: "Web Design",
+    category: "Web Development",
     viewDetails: "/",
   },
   {
-    image: cou3,
-    title: "Front End",
-    category: "eCommerce",
+    image: microsoft365,
+    title: "Microsoft 365",
+    category: "Productivity",
     viewDetails: "/",
   },
   {
-    image: cou4,
+    image: nodejs,
+    title: "Node.js Development",
+    category: "Web Development",
+    viewDetails: "/",
+  },
+  {
+    image: oracle,
+    title: "Oracle Database",
+    category: "Database Management",
+    viewDetails: "/",
+  },
+  {
+    image: powerapps,
+    title: "PowerApps Development",
+    category: "Productivity",
+    viewDetails: "/",
+  },
+  {
+    image: powerautomate,
+    title: "Power Automate",
+    category: "Productivity",
+    viewDetails: "/",
+  },
+  {
+    image: powerbi,
+    title: "Power BI",
+    category: "Data Visualization",
+    viewDetails: "/",
+  },
+  {
+    image: python,
+    title: "Python Programming",
+    category: "Development",
+    viewDetails: "/",
+  },
+  {
+    image: pythonfullstack,
     title: "Python Full Stack",
-    category: "Email Marketing",
+    category: "Web Development",
     viewDetails: "/",
   },
   {
-    image: cou1,
-    title: "Front End",
-    category: "Marketing & Reporting",
+    image: reactjs,
+    title: "React.js Development",
+    category: "Web Development",
     viewDetails: "/",
   },
   {
-    image: cou2,
-    title: "MERN Stack",
-    category: "App Development",
+    image: scrummaster,
+    title: "Scrum Master",
+    category: "Project Management",
     viewDetails: "/",
   },
   {
-    image: cou3,
-    title: "Python Full Stack",
-    category: "Email Marketing",
+    image: snowflask,
+    title: "Snowflake Data Warehousing",
+    category: "Data Engineering",
     viewDetails: "/",
   },
   {
-    image: cou4,
-    title: "Java Full Stack",
-    category: "Web Design",
+    image: sql,
+    title: "SQL Database",
+    category: "Database Management",
     viewDetails: "/",
   },
   {
-    image: cou1,
-    title: "Front End",
-    category: "App Development",
+    image: tableau,
+    title: "Tableau Data Visualization",
+    category: "Data Visualization",
     viewDetails: "/",
   },
   {
-    image: cou2,
-    title: "Python Full Stack",
-    category: "Email Marketing",
-    viewDetails: "/",
-  },
-  {
-    image: cou3,
-    title: "Python Full Stack",
-    category: "Email Marketing",
-    viewDetails: "/",
-  },
-  {
-    image: cou4,
-    title: "Python Full Stack",
-    category: "Email Marketing",
+    image: webdevelopement,
+    title: "Web Development",
+    category: "Web Development",
     viewDetails: "/",
   },
 ];
+
 
 const OurCourses: React.FC = () => {
   const settings = {
@@ -310,80 +250,82 @@ const OurCourses: React.FC = () => {
 
 
           <Swiper
-            autoHeight={false}
-            cssMode={true}
-            spaceBetween={20}
-            navigation={true}
-            pagination={{
-              clickable: true,
-            }}
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: true,
-              pauseOnMouseEnter: true,
-            }}
-            breakpoints={{
-              0: {
-                slidesPerView: 1,
-              },
-              576: {
-                slidesPerView: 2,
-              },
-              768: {
-                slidesPerView: 3,
-              },
-              1200: {
-                slidesPerView: 4,
-              },
-              1800: {
-                slidesPerView: 4,
-              },
-            }}
-            modules={[Autoplay, Navigation, Pagination]}
-            className="work-slides"
-          >
-            {ourWorksData &&
-              ourWorksData.map((value, i) => (
-                <SwiperSlide key={i}>
-                  <div className="work-card">
-                    <div className="custom-card">
-                    <Image
-                      src={value.image}
-                      alt="image"
-                      width={510}
-                      height={550}
-                    />
+  autoHeight={false}
+  cssMode={true}
+  spaceBetween={20}
+  navigation={true}
+  pagination={{
+    clickable: true,
+  }}
+  autoplay={{
+    delay: 2000,
+    disableOnInteraction: true,
+    pauseOnMouseEnter: true,
+  }}
+  breakpoints={{
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    1800: {
+      slidesPerView: 4,
+    },
+  }}
+  modules={[Autoplay, Navigation, Pagination]}
+  className="work-slides"
+>
+  {ourWorksData &&
+    ourWorksData.map((value, i) => (
+      <SwiperSlide key={i}>
+        <div className="work-card">
+          <div className="custom-card" style={{ position: 'relative' }}>
+            <div
+              style={{
+                width: '100%',
+                height: '300px',
+                position: 'relative',
+                overflow: 'hidden',
+                borderRadius: '8px',
+              }}
+            >
+              <Image
+                src={value.image}
+                alt="image"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <div className="content text-center">
+              <h3>
+                <Link href={value.viewDetails}>{value.title}</Link>
+              </h3>
+              <div
+                className="d-flex justify-content-center gap-2"
+                style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}
+              >
+                <Link href={value.viewDetails} className="custom-btn">
+                  Batches
+                </Link>
+                <Link href={value.viewDetails} className="custom-btn">
+                  Enroll Now
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+    ))}
+</Swiper>
 
-                    <div className="content text-center">
-                      {/* <span>
-                        <Link href={value.viewDetails}>{value.category}</Link>
-                      </span> */}
 
-                      <h3>
-                        <Link href={value.viewDetails}>{value.title}</Link>
-                      </h3>
-<div className="d-flex justify-content-center gap-2">
-                      <Link href={value.viewDetails} className="custom-btn">
-                      Batches
-                      </Link>
-                      <Link href={value.viewDetails} className="custom-btn">
-                      Enroll Now
-                      </Link>
-                      </div>
-                    </div>
-                  </div>
-                  </div>
-                  {/* <div className="mb-5">
-                    <Link href="#" className="text-white btn_des">
-                      Batches
-                    </Link>{" "}
-                    <Link href="#" className="text-white btn_des1 ms-2">
-                      Enroll Now
-                    </Link>
-                  </div> */}
-                </SwiperSlide>
-              ))}
-          </Swiper>
         </div>
       </section>
     </>
