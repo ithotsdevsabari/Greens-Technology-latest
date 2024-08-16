6e29dd4720e062f017436ab11bde1f65290325c6
sabarinathan N <sabarinathan25052001@gmail.com>
2024-08-17 01:27:21 +0530

    microsoft 365 added

--- src/components/Microsoft/UpcomingCourses/Beginners.tsx
+++ src/components/Microsoft/UpcomingCourses/Beginners.tsx
@@ -4,79 +4,76 @@ import Link from 'next/link';
 
 const Beginners: React.FC = () => {
   const [toggler, setToggler] = useState(false);
+
+  const courseData = [
+    {
+      date: "29 June 24",
+      course: "Microsoft 365",
+      city1: "AWST",
+      city2: "EST",
+      status: "Confirmed",
+      bookingLink: "#"
+    },
+    {
+      date: "29 July 24",
+      course: "Microsoft 365",
+      city1: "AWST",
+      city2: "EST",
+      status: "Confirmed",
+      bookingLink: "#"
+    },
+    {
+      date: "12 July 24",
+      course: "Microsoft 365",
+      city1: "AWST",
+      city2: "EST",
+      status: "Confirmed",
+      bookingLink: "#"
+    },
+    {
+      date: "13 July 24",
+      course: "Microsoft 365",
+      city1: "AWST",
+      city2: "EST",
+      status: "Confirmed",
+      bookingLink: "#"
+    },
+    {
+      date: "19 July 24",
+      course: "Microsoft 365",
+      city1: "AWST",
+      city2: "EST",
+      status: "Confirmed",
+      bookingLink: "#"
+    }
+  ];
+
   return (
-		<>  
-                <section>
-                    <div className="">
-                        <h2 className="text-center text-lg-start mt-0"> Microsoft 365 Batch Details</h2>
-                        <ul className="responsive-table pl-0">
-                            <li className="table-header">
-                                <div className="col col-01">Date</div>
-                                <div className="col col-02">Course</div>
-                                <div className="col col-03">AWST</div>
-                                <div className="col col-03">EST</div>
-                                <div className="col col-04">Status</div>
-                                <div className="col col-05">Booking</div>
-                            </li>
-                            <li className="table-row">
-                                <div className="col col-01" data-label="Date">29 June 24</div>
-                                <div className="col col-02" data-label="Course">Microsoft 365</div>
-                                <div className="col col-03" data-label="City">AWST</div>
-                                <div className="col col-03" data-label="City">EST</div>
-                                <div className="col col-04" data-label="Status">Confirmed</div>
-                                <div className="col col-05" data-label="Booking">
-                                    <Link className="fs-regular text-green-mid" href="#">Book Now</Link>
-                                </div>
-                            </li>    
-                            <li className="table-row">
-                                <div className="col col-01" data-label="Date">29 July 24</div>
-                                <div className="col col-02" data-label="Course">Microsoft 365</div>
-                                <div className="col col-03" data-label="City">AWST</div>
-                                <div className="col col-03" data-label="City">EST</div>
-                                <div className="col col-04" data-label="Status">Confirmed</div>
-                                <div className="col col-05" data-label="Booking">
-                                    <Link className="fs-regular text-green-mid" href="#">Book Now</Link>
-                                </div>
-                            </li>  
-                            <li className="table-row">
-                                <div className="col col-01" data-label="Date">12 July 24</div>
-                                <div className="col col-02" data-label="Course">Microsoft 365</div>
-                                <div className="col col-03" data-label="City">AWST</div>
-                                <div className="col col-03" data-label="City">EST</div>
-                                <div className="col col-04" data-label="Status">Confirmed</div>
-                                <div className="col col-05" data-label="Booking">
-                                    <Link className="fs-regular text-green-mid" href="#">Book Now</Link>
-                                </div>
-                            </li>   
-
-                            <li className="table-row">
-                                <div className="col col-01" data-label="Date">13 July 24</div>
-                                <div className="col col-02" data-label="Course">Microsoft 365</div>
-                                <div className="col col-03" data-label="City">AWST</div>
-                                <div className="col col-03" data-label="City">EST</div>
-                                <div className="col col-04" data-label="Status">Confirmed</div>
-                                <div className="col col-05" data-label="Booking">
-                                    <Link className="fs-regular text-green-mid" href="#">Book Now</Link>
-                                </div>
-                            </li>  
-
-                            <li className="table-row">
-                                <div className="col col-01" data-label="Date">19 July 24</div>
-                                <div className="col col-02" data-label="Course">Microsoft 365</div>
-                                <div className="col col-03" data-label="City">AWST</div>
-                                <div className="col col-03" data-label="City">EST</div>
-                                <div className="col col-04" data-label="Status">Confirmed</div>
-                                <div className="col col-05" data-label="Booking">
-                                    <Link className="fs-regular text-green-mid" href="#">Book Now</Link>
-                                </div>
-                            </li>  
-                        </ul>
-                    </div>
-                </section>
-
-
-</>
-    );
-}
+    <section>
+      <div>
+        <h2 className="text-center text-lg-start mt-0">Microsoft 365 Batch Details</h2>
+        <ul className="responsive-table pl-0">
+          <li className="table-header">
+            <div className="col col-02">Course</div>
+            <div className="col col-04">Status</div>
+            <div className="col col-05">Booking</div>
+          </li>
+          {courseData.map((course, index) => (
+            <li key={index} className="table-row">
+              {/* <div className="col col-01" data-label="Date">{course.date}</div> */}
+              <div className="col col-02" data-label="Course">{course.course}</div>
+              {/* <div className="col col-03" data-label="City">{course.city1}</div> */}
+              {/* <div className="col col-03" data-label="City">{course.city2}</div> */}
+              <div className="col col-04" data-label="Status">{course.status}</div>
+              <div className="col col-05" data-label="Booking">
+                <Link className="fs-regular text-green-mid" href={course.bookingLink}>Book Now</Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Beginners;
