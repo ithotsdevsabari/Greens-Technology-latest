afda5f026108c33d19552d57689431b6276f51ec
ithotsDev02 <ithotsdev02@gmail.com>
2024-08-22 13:57:34 +0530

    Whatsapp number changed

--- src/components/Atom/WhatsappConfiguration.tsx
+++ src/components/Atom/WhatsappConfiguration.tsx
@@ -5,7 +5,7 @@ import WhatsApp from "../../../public/images/whatsapp-chat.png";
 const WhatsappConfiguration = () => {
 
   const whatsappLink =
-    "https://api.whatsapp.com/send?phone=918428599646&text=Hello! I am interested in learning more about the IT training programs offered by Greens Technology. Could you please provide more details on the courses available, schedules, and fees? Thank you!";
+    "https://api.whatsapp.com/send?phone=61423737907&text=Hello! I am interested in learning more about the IT training programs offered by Greens Technology. Could you please provide more details on the courses available, schedules, and fees? Thank you!";
 
   return (
     <>
